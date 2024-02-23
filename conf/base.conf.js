4c00d087e8ef25be535402ea787debea9989c54b
divaharg180 <divaharg180@gmail.com>
2024-02-23 17:28:15 +0530

    android and ios selenium upload and push file

---
+++ conf/base.conf.js
@@ -0,0 +1,32 @@
+exports.config = {
+  user: "sekarmanoj_XeuqF4",
+  key: "ceKAXjhwv15aHzPBm6pE",
+
+  updateJob: false,
+  specs: [
+    './tests/specs/mobileUpload.android.push.js',
+    "./tests/specs/mobileUpload.test.js",
+  ],
+  exclude: [],
+
+  logLevel: "warn",
+  coloredLogs: true,
+  screenshotPath: "./errorShots/",
+  baseUrl: "",
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 120000,
+  connectionRetryCount: 3,
+  hostname: "hub.browserstack.com",
+  services: [["browserstack"]],
+
+  before: function () {
+    var chai = require("chai");
+    global.expect = chai.expect;
+    chai.Should();
+  },
+  framework: "mocha",
+  mochaOpts: {
+    ui: "bdd",
+    timeout: 60000,
+  },
+};
